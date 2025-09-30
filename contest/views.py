21d6f4e2c361ae3d148dd05ddaf085bc20c23fef
Apple <apple@Apples-MacBook-Pro.local>
2025-09-30 18:40:39 +0530

    some test changes

--- contest/views.py
+++ contest/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Prefetch
+# from django.db.models import Prefetch
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
